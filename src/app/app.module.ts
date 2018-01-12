94fc8b753c1a7e47d4ed4dac0c121139ed063817
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-01-12 18:18:39 -0500

    Segunda app en Angular - SPA - Se crearon los componentes about, heroes, se agregaron las imagenesy se modifico el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 
@@ -13,7 +15,9 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
